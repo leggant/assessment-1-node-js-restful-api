d4807b313b8083711b14805ff4f6ba677964a4be
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-06 19:06:07 +1300

    refactor(api/v1/routes/auth.js): add login schema validation
    created validator for the user login
    
    BREAKING CHANGE: currently requires both username and email, needs to be set to be one field or the
    other
    
    re #9

--- api/v1/routes/auth.js
+++ api/v1/routes/auth.js
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-import-module-exports */
-import express, { Router } from "express";
+import { Router } from "express";
 import PATHS from "../constants/paths.js";
 import { register, login } from "../controllers/auth_controller.js";
 import registerSchema from "../schemas/register-user-schema.js";
+import LoginSchema from "../schemas/login-user-schema.js";
 import validateSchema from "../middleware/mw_validateSchema.js";
 
 const router = Router();
 
 router.post(PATHS.REGISTER, registerSchema, validateSchema, register);
-router.post(PATHS.LOGIN, login);
+router.post(PATHS.LOGIN, LoginSchema, validateSchema, login);
 
 export default router;

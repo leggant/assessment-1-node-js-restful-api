0fb118e5fe2f3d8cd6576931267ff317a3bafbd4
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-14 12:20:27 +1300

    refactor(utils/requests.mjs): create prisma request module
    created reusable prisma requests that can be import and used throughout the API
    
    re #20, re #23, re #36

--- api/v1/constants/paths.js
+++ api/v1/constants/paths.js
@@ -2,6 +2,15 @@ const PATHS = {
   AUTH: "user/auth",
   REGISTER: "/register",
   LOGIN: "/login",
+  ADMIN: {
+    METHODS: ["GET", "CREATE", "UPDATE", "DELETE"],
+    ADMINSEEDER: "user/auth/admin/seeder",
+    ADMINQUIZ: "user/auth/admin/quiz",
+  },
+  BASIC: {
+    METHODS: ["GET", "CREATE"],
+    USERQUIZ: "user/auth/quiz",
+  },
 };
 
 export default PATHS;

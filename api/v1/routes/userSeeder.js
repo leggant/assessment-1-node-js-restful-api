957aea317552a5d1768fb7e2375bf29f714c9162
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-21 18:24:34 +1300

    refactor(api/v1/routes/userseeder.js): add token validation middleware
    deleted admin users with valid tokens are now blocked from using the user table seeder through the
    addition of the mwTokenValid middleware
    
    re #20

--- api/v1/routes/userSeeder.js
+++ api/v1/routes/userSeeder.js
@@ -3,12 +3,14 @@ import PATHS from "../constants/paths.js";
 import { seedUsers } from "../controllers/seeder_controller.js";
 import mwAuth from "../middleware/mw_authentication.js";
 import mwAdminUser from "../middleware/mw_adminUser.js";
+import mwTokenValid from "../middleware/mw_tokenValid.js";
 
 const userSeederRouter = Router();
 // user/auth/admin/seeder/users
 userSeederRouter.get(
   `${PATHS.ADMIN.ADMINSEEDER}`,
   mwAuth,
+  mwTokenValid,
   mwAdminUser,
   seedUsers,
 );

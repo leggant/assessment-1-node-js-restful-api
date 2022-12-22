939ea85e646a3f7f98b4742560458b8f0aba2c6d
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-22 17:40:05 +1300

    fix(mw_tokenvalid.js): check if basic user deleted by admin
    updated the middleware to utilise the new BlockedUser model, which temporarily stores a user who has
    been deleted by an admin; this check will block a user who may still have a valid token, but no
    entry in the user table
    
    re #20

--- api/v1/middleware/mw_tokenValid.js
+++ api/v1/middleware/mw_tokenValid.js
@@ -1,5 +1,7 @@
+import jwt from "jsonwebtoken";
 import PRISMA from "../../../utils/prisma.mjs";
 import checkDataType from "../../../utils/checkDataType.js";
+import USERTYPE from "../constants/userType.js";
 
 const mwTokenValid = async (req, res, next) => {
   try {
@@ -8,11 +10,30 @@ const mwTokenValid = async (req, res, next) => {
     const checkBlockedToken = await PRISMA.blockedToken.findFirst({
       where: { token },
     });
-    const type = checkDataType(checkBlockedToken);
+    let type = checkDataType(checkBlockedToken);
     if (type === "object") {
       return res.status(403).json({
         msg: "Authentication Token Is Invalid.",
       });
+    }
+    // if the data type is not an object, it will be null
+    // next get the token payload
+    const tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+    // check: is the user associated with the token a basic user
+    // and is this basic user in the BlockedUsers
+    // table i.e deleted by Admin User
+    // if this is the case, any token issued to this basic user
+    // within the last hour will still be valid
+    if (tokenPayload.role === USERTYPE.BASIC) {
+      const checkBlockedUser = await PRISMA.blockedUser.findFirst({
+        where: { uid: tokenPayload.id },
+      });
+      type = checkDataType(checkBlockedUser);
+      if (type === "object") {
+        return res.status(403).json({
+          msg: "Account Suspended/Deleted By Admin",
+        });
+      }
     }
     return next();
   } catch (error) {

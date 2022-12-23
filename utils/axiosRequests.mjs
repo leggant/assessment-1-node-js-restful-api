3061e66173182d1c518f2856b6d0f49c7c3f1d95
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-23 18:07:31 +1300

    docs(docs/*.html): update jsDocs
    add utils modules axiosRequests.mjs and prisma.mjs
    
    re #13

--- utils/axiosRequests.mjs
+++ utils/axiosRequests.mjs
@@ -1,11 +1,30 @@
 import axios from "axios";
-
+/**
+ * create a reusable instance of axios, complete with base url, and allowed methods
+ * @constructor axios
+ * @version 1.2.0
+ * @constant GISTAXIOS instance of axios created to target seeder endpoints
+ * @description create a reusable instance of axios, complete with base url, and allowed methods
+ * @see {@link https://axios-http.com/docs/instance|axios-http}
+ * @example const instance = axios.create({
+baseURL: 'https://some-domain.com/api/',
+method: "GET",
+timeout: 5000,
+headers: {'X-Custom-Header': 'foobar'}
+});
+ */
 const GISTAXIOS = axios.create({
   baseURL:
     "https://gist.githubusercontent.com/leggant/0bba24ff5402123c0a1301df853c5541/raw/1749009d0d9ef6d14dd67939d6d5b734a2621c0c",
   method: "GET",
 });
-
+/**
+ * gist endpoint for admin user seeder
+ * @const GISTADMIN
+ * @type {string}
+ * @description gist endpoint for admin user seeder
+ * @default "/admin_user.json"
+ */
 const GISTADMIN = "/admin_user.json";
 const GISTBASIC = "/basic_user.json";
 
@@ -29,4 +48,4 @@ const quizAxios = axios.create({
   method: "GET",
 });
 
-export { GISTAXIOS, quizAxios, getUsers };
+export { quizAxios, getUsers };

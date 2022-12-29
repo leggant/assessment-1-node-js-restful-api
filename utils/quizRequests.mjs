87eff33afca03421193d2b9ff933651f170785e4
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-29 16:58:48 +1300

    refactor(utils/quizrequests.js): create quiz questions associated with new quiz
    method called following the successful creation of a new quiz; data is requested from the opentdb
    api; questions are mapped over and created individually
    
    re #36

--- utils/quizRequests.mjs
+++ utils/quizRequests.mjs
@@ -3,29 +3,11 @@
  * @module quizRequests
  */
 import PRISMA from "./prisma.mjs";
-import USERTYPE from "../api/v1/constants/userType.js";
-import checkDataType from "./checkDataType.js";
-import checkIfObjectIsEmpty from "./checkEmptyObject.js";
 import { dbDateStringFromDate } from "./dateTimeCheck.mjs";
 
 const createNewQuiz = async (reqdata) => {
-  const {
-    categoryId,
-    name,
-    startDate,
-    endDate,
-    difficulty,
-    answerType,
-    questions,
-  } = reqdata;
-  console.log(
-    startDate,
-    endDate,
-    categoryId,
-    difficulty,
-    answerType,
-    questions,
-  );
+  const { categoryId, name, startDate, endDate, difficulty, answerType } =
+    reqdata;
   const dbStartDate = dbDateStringFromDate(startDate);
   const dbEndDate = dbDateStringFromDate(endDate);
   const newQuiz = await PRISMA.quiz.create({
@@ -41,8 +23,43 @@ const createNewQuiz = async (reqdata) => {
   return newQuiz;
 };
 
-const createNewQuizQuestions = async (reqdata) => {
-  console.log(reqdata);
+const createNewQuizQuestions = async (QUIZDATA, QUIZINFO, res) => {
+  Promise.all(
+    QUIZDATA.map((q) => {
+      const createquestions = PRISMA.question.create({
+        data: {
+          quizId: QUIZINFO.id,
+          question: q.question,
+          correctAnswer: q.correct_answer,
+          incorrectAnswers: q.incorrect_answers,
+        },
+      });
+      return createquestions;
+    }),
+  )
+    .then((data) => {
+      const count = Object.keys(data).length;
+      console.info(`${count} questions added to quiz ${QUIZINFO.name}`);
+      res.status(201).json({
+        msg: `${QUIZINFO.name} Successfully Created.`,
+        data: {
+          id: QUIZINFO.id,
+          categoryId: QUIZDATA.categoryId,
+          name: QUIZINFO.name,
+          questionCount: count,
+          difficulty: QUIZINFO.difficulty,
+          answerType: QUIZINFO.answerType,
+          quizStartDate: QUIZINFO.startDate,
+          quizEndDate: QUIZINFO.endDate,
+        },
+      });
+    })
+    .catch((e) => {
+      console.error(`Failed to create questions for quiz: ${QUIZINFO.name}`, e);
+      res
+        .status(401)
+        .json({ msg: `Failed to create questions for quiz: ${QUIZINFO.name}` });
+    });
 };
 
 const createNewQuizAnswers = async (reqdata) => {

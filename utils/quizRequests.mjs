a87523c9825ecdd6c173084e7515dbbb0e4458ad
Anthony Legg <leggtc1@student.op.ac.nz>
2022-12-29 14:09:50 +1300

    refactor(utils/quizrequests.mjs): create quiz using prisma
    created a function to make an entry in the database for a new quiz, using the data passed in the
    request object. Return id, quiz will be used to add questions and answer table entries
    
    re #36

--- utils/quizRequests.mjs
+++ utils/quizRequests.mjs
@@ -6,9 +6,57 @@ import PRISMA from "./prisma.mjs";
 import USERTYPE from "../api/v1/constants/userType.js";
 import checkDataType from "./checkDataType.js";
 import checkIfObjectIsEmpty from "./checkEmptyObject.js";
-import { createQuizRequest } from "./axiosRequests.mjs";
+import { dbDateStringFromDate } from "./dateTimeCheck.mjs";
 
-const createNewQuiz = async (reqdata) => {};
+const createNewQuiz = async (reqdata) => {
+  const {
+    categoryId,
+    name,
+    startDate,
+    endDate,
+    difficulty,
+    answerType,
+    questions,
+  } = reqdata;
+  console.log(
+    startDate,
+    endDate,
+    categoryId,
+    difficulty,
+    answerType,
+    questions,
+  );
+  const dbStartDate = dbDateStringFromDate(startDate);
+  const dbEndDate = dbDateStringFromDate(endDate);
+  const newQuiz = await PRISMA.quiz.create({
+    data: {
+      categoryId,
+      name,
+      answerType,
+      difficulty,
+      startDate: dbStartDate,
+      endDate: dbEndDate,
+    },
+  });
+  return newQuiz;
+};
+
+const createNewQuizQuestions = async (reqdata) => {
+  console.log(reqdata);
+};
+
+const createNewQuizAnswers = async (reqdata) => {
+  console.log(reqdata);
+};
+
+const addQuizPlayers = async (reqdata) => {
+  console.log(reqdata);
+};
 
 // eslint-disable-next-line import/prefer-default-export
-export { createNewQuiz };
+export {
+  createNewQuiz,
+  createNewQuizQuestions,
+  createNewQuizAnswers,
+  addQuizPlayers,
+};

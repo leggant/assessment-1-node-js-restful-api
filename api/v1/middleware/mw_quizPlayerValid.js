d0e8451cb73d92aee95d6bc4ca562f121b9f7c86
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-06 17:42:52 +1300

    refactor(mw_quizplayvalid.js): check the quiz date is valid
    use the playerCanParticipate utility function to check the quiz date is within the allowed range,
    return errors if the user is already assigned to the quiz or if the current date is outside the
    quizzes start and end date
    
    re #47

--- api/v1/middleware/mw_quizPlayerValid.js
+++ api/v1/middleware/mw_quizPlayerValid.js
@@ -1,11 +1,7 @@
+import moment from "moment";
 import USERTYPE from "../constants/userType.js";
 import PRISMA from "../../../utils/prisma.js";
-import checkIfObjectIsEmpty from "../../../utils/checkEmptyObject.js";
-import {
-  playerCanParticipate,
-  splitDate,
-  dbDateStringFromDate,
-} from "../../../utils/dateTimeCheck.js";
+import { playerCanParticipate } from "../../../utils/dateTimeCheck.js";
 
 const mwQuizPlayerValid = async (req, res, next) => {
   try {
@@ -32,20 +28,27 @@ const mwQuizPlayerValid = async (req, res, next) => {
       // if the user is ok to add to the quiz,
       // check the dates of the quiz to ensure the current date is with a valid range
       if (userCanBeAdded) {
-        console.log(validQuiz.startDate);
-        // const qStart = dbDateStringFromDate(validQuiz.startDate);
-        // const qEnd = dbDateStringFromDate(validQuiz.endDate);
-        // const quizDatesOk = await playerCanParticipate(qStart, qEnd);
-        // req.player = {
-        //   playerId: req.user.id,
-        //   quizId,
-        // };
-        // console.log(quizDatesOk.valueOf());
-        return next();
+        const quizDatesOk = await playerCanParticipate(
+          validQuiz.startDate,
+          validQuiz.endDate,
+        );
+        if (quizDatesOk) {
+          req.quizPlayer = {
+            quizId,
+            playerId: req.user.id,
+          };
+        } else if (!quizDatesOk) {
+          throw new Error(
+            `QuiZ Id #${quizId} has expired. ${moment()} is not within: ${validQuiz.startDate.toDateString()}-${validQuiz.endDate.toDateString()}`,
+          );
+        }
+      } else {
+        throw new Error("User Is Already A Participant In The Quiz.");
       }
-      throw new Error("User Is Already A Participant In The Quiz.");
+    } else {
+      throw new Error(`Quiz With An ID of #${quizId} Was Not Found.`);
     }
-    throw new Error(`Quiz With An ID of #${quizId} Was Not Found.`);
+    return next();
   } catch (error) {
     return res.status(404).json({ msg: error.message });
   }

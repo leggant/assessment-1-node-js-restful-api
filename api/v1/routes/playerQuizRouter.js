7bcd240c985e5bf0739355c2a4bd38cb5a9d930f
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-08 15:47:54 +1300

    refactor(api/v1/routes/playerquizrouter.js): add route to get all incomplete quizzes
    end point added for testing/debugging; if complete will leave in the final submission;
    
    BREAKING CHANGE: currently, the query on this end point is not working correctly
    
    re #47

--- api/v1/routes/playerQuizRouter.js
+++ api/v1/routes/playerQuizRouter.js
@@ -2,22 +2,42 @@ import { Router } from "express";
 import PATHS from "../constants/paths.js";
 import validateSchema from "../middleware/mw_validateSchema.js";
 
-import { ctAddQuizPlayer } from "../controllers/quizPlayerController.js";
+import {
+  ctAddQuizPlayer,
+  ctSubmitQuizAnswers,
+  ctGetPlayersIncompleteQuizzes,
+} from "../controllers/quizPlayerController.js";
 
 import mwAuth from "../middleware/mw_authentication.js";
 import mwTokenValid from "../middleware/mw_tokenValid.js";
 import mwQuizPlayerValid from "../middleware/mw_quizPlayerValid.js";
+import PlayerSubmitQuizAnswersSchema from "../schemas/player-answer-question-schema.js";
 
 const router = Router();
+
+router.get(
+  PATHS.QUIZ.PLAYERUNFINISHEDQUIZZES,
+  mwAuth,
+  mwTokenValid,
+  ctGetPlayersIncompleteQuizzes,
+);
 
 router.post(
   PATHS.QUIZ.PLAYER,
   mwAuth,
   mwTokenValid,
   mwQuizPlayerValid,
-  // AddPlayerToQuizSchema,
-  // validateSchema,
   ctAddQuizPlayer,
+);
+
+router.post(
+  PATHS.QUIZ.SUBMITANSWERS,
+  mwAuth,
+  mwTokenValid,
+  mwQuizPlayerValid,
+  PlayerSubmitQuizAnswersSchema,
+  validateSchema,
+  ctSubmitQuizAnswers,
 );
 
 export default router;

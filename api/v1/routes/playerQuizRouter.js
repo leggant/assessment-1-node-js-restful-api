16cf9b83d9eb456b167fa520e6a6985c1aba714b
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-10 20:54:58 +1300

    refactor(api/v1/routes/playerquizrouter.js): add new middlewares to answer post route
    updated the post request to the quiz answer submission end point; these verify the player has been
    added to a quiz, and the current date/quiz start/end dates are within a valid range
    
    re #47

--- api/v1/routes/playerQuizRouter.js
+++ api/v1/routes/playerQuizRouter.js
@@ -11,6 +11,8 @@ import {
 import mwAuth from "../middleware/mw_authentication.js";
 import mwTokenValid from "../middleware/mw_tokenValid.js";
 import mwQuizPlayerValid from "../middleware/mw_quizPlayerValid.js";
+import mwPlayerAddedToQuiz from "../middleware/mw_playerAddedToQuiz.js";
+import mwCurrentDateValid from "../middleware/mw_currentDateValid.js";
 import PlayerSubmitQuizAnswersSchema from "../schemas/player-answer-question-schema.js";
 
 const router = Router();
@@ -34,9 +36,8 @@ router.post(
   PATHS.QUIZ.SUBMITANSWERS,
   mwAuth,
   mwTokenValid,
-  mwQuizPlayerValid,
-  PlayerSubmitQuizAnswersSchema,
-  validateSchema,
+  mwPlayerAddedToQuiz,
+  mwCurrentDateValid,
   ctSubmitQuizAnswers,
 );
 

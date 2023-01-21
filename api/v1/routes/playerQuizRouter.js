a616c13bc2c8ca9a3549c54d6c01b32f339cc787
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-22 11:08:44 +1300

    refactor(api/v1/routes/playerquizrouter.js): remove incomplete quizzes endpoint
    re #47

--- api/v1/routes/playerQuizRouter.js
+++ api/v1/routes/playerQuizRouter.js
@@ -6,27 +6,20 @@ import {
   ctAddQuizPlayer,
   ctGetQuizQuestions,
   ctSubmitQuizAnswers,
-  ctGetPlayersIncompleteQuizzes,
 } from "../controllers/quizPlayerController.js";
 
+import mwQuizValid from "../middleware/mw_quizValid.js";
 import mwAuth from "../middleware/mw_authentication.js";
 import mwTokenValid from "../middleware/mw_tokenValid.js";
 import mwQuizPlayerValid from "../middleware/mw_quizPlayerValid.js";
 import mwPlayerAddedToQuiz from "../middleware/mw_playerAddedToQuiz.js";
 import mwCurrentDateValid from "../middleware/mw_currentDateValid.js";
-import mwQuizValid from "../middleware/mw_quizValid.js";
 
-import PlayerSubmitQuizAnswersSchema from "../schemas/player-submit-answers-schema.js";
 import QuizParamSchema from "../schemas/param-quiz-schema.js";
+import PlayerSubmitQuizAnswersSchema from "../schemas/player-submit-answers-schema.js";
 
 const router = Router();
 
-router.get(
-  PATHS.QUIZ.PLAYERUNFINISHEDQUIZZES,
-  mwAuth,
-  mwTokenValid,
-  ctGetPlayersIncompleteQuizzes,
-);
 // add player to a quiz
 router.post(
   PATHS.QUIZ.PLAYER,

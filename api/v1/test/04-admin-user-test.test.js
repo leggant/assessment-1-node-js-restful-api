1265cd0445841689438bcbffb9b01176942ba69b
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-26 22:34:25 +1300

    test(04-admin-user-test.test.js): add error response validation
    validate error response object with `.to.deep.equal()`
    
    re #14

--- api/v1/test/04-admin-user-test.test.js
+++ api/v1/test/04-admin-user-test.test.js
@@ -8,6 +8,7 @@ import { ADMINTESTUSER } from "../../../utils/unitTestDataRequests.js";
 chai.use(chaiHttp);
 
 const BASE = "/api/v1";
+let token;
 
 describe("Admin User Authentication and Auth Request Validation", () => {
   it("should login admin user with valid input", (done) => {
@@ -75,20 +76,37 @@ describe("Admin User Authentication and Auth Request Validation", () => {
         chai
           .expect(res.body.errors[0])
           .to.have.all.keys("value", "msg", "param", "location");
-        chai.expect(res.body.errors[0]).to.include(
-          {
-            value: "fail",
-            msg: "Password must have at least 8 characters, one number and one special character",
-            param: "password",
-            location: "body",
-          },
-          {
-            value: "fail",
-            msg: "Password must be 8 char min and 16 char max lengths",
-            param: "password",
-            location: "body",
-          },
+        chai.expect(res.body.errors).to.deep.equal(
+          [
+            {
+              value: "fail",
+              msg: "Password must have at least 8 characters, one number and one special character",
+              param: "password",
+              location: "body",
+            },
+            {
+              value: "fail",
+              msg: "Password must be 8 char min and 16 char max lengths",
+              param: "password",
+              location: "body",
+            },
+          ],
+          "correct error params passed",
         );
+        // chai.expect(res.body.errors[0]).to.include(
+        //   {
+        //     value: "fail",
+        //     msg: "Password must have at least 8 characters, one number and one special character",
+        //     param: "password",
+        //     location: "body",
+        //   },
+        //   {
+        //     value: "fail",
+        //     msg: "Password must be 8 char min and 16 char max lengths",
+        //     param: "password",
+        //     location: "body",
+        //   },
+        // );
         done();
       });
   });

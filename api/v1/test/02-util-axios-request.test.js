1506bb387d7b8b16865aa0561359fe801a8f8f89
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-29 01:50:16 +1300

    test(02-util-axios-request.test.js): add test for user seeder gist
    test end point for the user gist endpoint
    
    re #6, re #14, re #59

--- api/v1/test/02-util-axios-request.test.js
+++ api/v1/test/02-util-axios-request.test.js
@@ -1,37 +1,57 @@
 import { assert, expect } from "chai";
 import { describe, it } from "mocha";
-import moment from "moment";
-import * as dateparser from "../../../utils/dateTimeCheck.js";
+import * as axios from "../../../utils/axiosRequests.js";
 
-/**
- * @constant {{ split: String, quizStart: String, quizEnd: String }} testDates
- */
-const testDates = { split: "2023-01-10", quizStart: "", quizEnd: "" };
-const testNow = dateparser.createMomentDate();
-
-describe("Test dateTimeCheck.js Utility Functions", () => {
-  it("test split date string function", (done) => {
-    const vals = dateparser.splitDate(testDates.split);
-    assert.containsAllKeys(
-      vals,
-      ["year", "month", "day"],
-      "An Error Occured, Split String Keys Not Returned",
-    );
+describe("Test Gist Seeder endpoints with axioRequests.js Util Functions", () => {
+  it("test gist endpoint to get user data", (done) => {
+    const userReq = new Promise((resolve) => {
+      resolve(axios.getUsers());
+    });
+    userReq.then((res) => {
+      assert.isArray(res.users);
+    });
     done();
   });
-  it("Validate Quiz Date/Current Date Are Within A Valid Range", (done) => {
-    const qStart = testNow.add(2, "day").toDate();
-    assert.isTrue(
-      dateparser.quizDateValid(qStart),
-      "Quiz Date Does Not Start After The Current Date",
-    );
-    done();
-  });
-  it("Gets UTC Date From A String", (done) => {
-    const xday = testNow.add(2, "day").format("Y-M-D");
-    const result = dateparser.dbDateStringFromDate(xday);
-    assert.isTrue(moment.isDate(result), "Returned Value Is Not A Valid Date");
-    assert.isOk(dateparser.dbDateStringFromDate(xday));
-    done();
-  });
+  // it("test split date string function", (done) => {
+  //   const vals = dateparser.splitDate("2023-01-10");
+  //   assert.containsAllKeys(
+  //     vals,
+  //     ["year", "month", "day"],
+  //     "An Error Occured, Split String Keys Not Returned",
+  //   );
+  //   done();
+  // });
+  // it("Validate Quiz Date/Current Date Are Within A Valid Range", (done) => {
+  //   const qStart = moment().add(2, "day").toDate();
+  //   assert.isTrue(
+  //     dateparser.quizDateValid(qStart),
+  //     "Quiz Date Does Not Start After The Current Date",
+  //   );
+  //   done();
+  // });
+  // it("Creates a UTC Date From A String", (done) => {
+  //   const xday = moment().add(2, "day").format("Y-M-D");
+  //   const result = dateparser.dbDateStringFromDate(xday);
+  //   assert.isTrue(moment.isDate(result), "Returned Value Is Not A Valid Date");
+  //   assert.isOk(dateparser.dbDateStringFromDate(xday));
+  //   done();
+  // });
+  // it("Check That The Current Date Falls Between the Quiz Start/End Dates", (done) => {
+  //   const startOk = moment().subtract(1, "day").toDate();
+  //   const endOk = moment().add(2, "days").toDate();
+  //   assert.isTrue(
+  //     dateparser.playerCanParticipate(startOk, endOk),
+  //     "Current Date Is Not Within A Valid Range",
+  //   );
+  //   done();
+  // });
+  // it("Check That The Quiz playerCanParticipate() Date Validation Returns Errors", (done) => {
+  //   const startNotOk = moment().subtract(20, "days").toDate();
+  //   const endNotOk = moment().subtract(10, "days").toDate();
+  //   assert.isFalse(
+  //     dateparser.playerCanParticipate(startNotOk, endNotOk),
+  //     "Current Date Is Within A Valid Range, The Test Should Return False",
+  //   );
+  //   done();
+  // });
 });

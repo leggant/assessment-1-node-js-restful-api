bbd9b00ba0467d972aaa1c45ef5c3fcb7823f755
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-28 15:42:02 +1300

    test(10-util-date-parser.test.js): test playerCanParticipate function
    check case for both valid and invalid date ranges
    
    re #6

--- api/v1/test/10-util-date-parser.test.js
+++ api/v1/test/10-util-date-parser.test.js
@@ -7,7 +7,6 @@ import * as dateparser from "../../../utils/dateTimeCheck.js";
  * @constant {{ split: String, quizStart: String, quizEnd: String }} testDates
  */
 const testDates = { split: "2023-01-10", quizStart: "", quizEnd: "" };
-const testNow = dateparser.createMomentDate();
 
 describe("Test dateTimeCheck.js Utility Functions", () => {
   it("test split date string function", (done) => {
@@ -20,18 +19,36 @@ describe("Test dateTimeCheck.js Utility Functions", () => {
     done();
   });
   it("Validate Quiz Date/Current Date Are Within A Valid Range", (done) => {
-    const qStart = testNow.add(2, "day").toDate();
+    const qStart = moment().add(2, "day").toDate();
     assert.isTrue(
       dateparser.quizDateValid(qStart),
       "Quiz Date Does Not Start After The Current Date",
     );
     done();
   });
-  it("Gets UTC Date From A String", (done) => {
-    const xday = testNow.add(2, "day").format("Y-M-D");
+  it("Creates a UTC Date From A String", (done) => {
+    const xday = moment().add(2, "day").format("Y-M-D");
     const result = dateparser.dbDateStringFromDate(xday);
     assert.isTrue(moment.isDate(result), "Returned Value Is Not A Valid Date");
     assert.isOk(dateparser.dbDateStringFromDate(xday));
+    done();
+  });
+  it("Check That The Current Date Falls Between the Quiz Start/End Dates", (done) => {
+    const startOk = moment().subtract(1, "day").toDate();
+    const endOk = moment().add(2, "days").toDate();
+    assert.isTrue(
+      dateparser.playerCanParticipate(startOk, endOk),
+      "Current Date Is Not Within A Valid Range",
+    );
+    done();
+  });
+  it("Check That The Quiz playerCanParticipate() Date Validation Returns Errors", (done) => {
+    const startNotOk = moment().subtract(20, "days").toDate();
+    const endNotOk = moment().subtract(10, "days").toDate();
+    assert.isFalse(
+      dateparser.playerCanParticipate(startNotOk, endNotOk),
+      "Current Date Is Within A Valid Range, The Test Should Return False",
+    );
     done();
   });
 });

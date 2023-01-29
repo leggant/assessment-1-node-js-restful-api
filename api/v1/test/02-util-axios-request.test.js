c05d0c2dd4077d52b042ee58b824cfadc1bd9c68
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-29 16:26:57 +1300

    test(02-util-axios-request.test.js): complete test for user gist
    re #59, re #14, re #6

--- api/v1/test/02-util-axios-request.test.js
+++ api/v1/test/02-util-axios-request.test.js
@@ -1,6 +1,7 @@
-import { assert, expect, should } from "chai";
+import { assert, expect } from "chai";
 import { describe, it, before } from "mocha";
 import * as axios from "../../../utils/axiosRequests.js";
+import USERTYPE from "../constants/userType.js";
 
 describe("Test Gist Seeder endpoints with axioRequests.js Util Functions", () => {
   let userData;
@@ -14,11 +15,32 @@ describe("Test Gist Seeder endpoints with axioRequests.js Util Functions", () =>
       done();
     });
     it("Test The Data Has Required Keys", (done) => {
-      assert.isObject(...userData);
+      expect(...userData).to.have.all.keys([
+        "firstName",
+        "lastName",
+        "userName",
+        "email",
+        "role",
+        "password",
+        "confirmPassword",
+      ]);
       done();
     });
-    it("Test If The Return Has An Error Message", (done) => {
-      assert.isObject(...userData);
+    it("Test User Roles Are Of The Correct Type", (done) => {
+      expect(...userData)
+        .to.have.property("role")
+        .that.is.oneOf([USERTYPE.ADMIN, USERTYPE.BASIC]);
+      done();
+    });
+    it("Test User Passwords Conform to Regex Requirements", (done) => {
+      expect(...userData.map((data) => data))
+        .to.have.property("password")
+        .to.match(/^(?=.*[0-9])(?=.*[!@#$%^&amp;*])(?=\S{8,16}$).*$/);
+      done();
+    });
+    it("Test Usernames and Email Addresses match", (done) => {
+      const check = userData.every((obj) => obj.email.startsWith(obj.userName));
+      assert.isTrue(check);
       done();
     });
   });

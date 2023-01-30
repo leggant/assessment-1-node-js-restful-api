fd642d560a4f5273b256eeb98cbe1545f2b52e7c
Anthony Legg <leggtc1@student.op.ac.nz>
2023-01-30 20:13:10 +1300

    test(api/v1/test/*): add outline of quiz tests to create x17
    17 quiz tests planned to be implemented
    
    re #6

--- api/v1/test/07-admin-user-quiz.test.js
+++ api/v1/test/07-admin-user-quiz.test.js
@@ -10,4 +10,14 @@ chai.use(chaiHttp);
 const BASE = "/api/v1";
 let token;
 
-describe("Admin User Quiz Request Tests", () => {});
+describe("Admin User Quiz Request Tests", () => {
+  describe("Create Quiz Request Test", () => {});
+  describe("Update Quiz Request Test", () => {});
+  describe("Delete Quiz Request Test", () => {});
+  describe("Admin User Quiz Request Error Handling Tests", () => {
+    describe("Admin Play Quiz Request Test w/ Errors", () => {});
+    describe("Create Quiz Request Test w/ Errors", () => {});
+    describe("Update Quiz Request Test w/ Errors", () => {});
+    describe("Delete Quiz Request Test w/ Errors", () => {});
+  });
+});
--- api/v1/test/08-basic-user-quiz.test.js
+++ api/v1/test/08-basic-user-quiz.test.js
@@ -10,4 +10,13 @@ chai.use(chaiHttp);
 const BASE = "/api/v1";
 let token;
 
-describe("Basic User Quiz Request Tests", () => {});
+describe("Basic User Quiz Request Tests", () => {
+  describe("Sign On To Play A Quiz Request Test", () => {});
+  describe("Submit Quiz Answers Request Test", () => {});
+  describe("Delete Quiz Request Test", () => {});
+  describe("Admin User Quiz Request Error Handling Tests", () => {
+    describe("Create Quiz Request Test w/ Errors", () => {});
+    describe("Update Quiz Request Test w/ Errors", () => {});
+    describe("Delete Quiz Request Test w/ Errors", () => {});
+  });
+});

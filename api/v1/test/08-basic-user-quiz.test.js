f89abcfa0ea4d6ce5edaab9c9697e8a4e933995b
Anthony Legg <leggtc1@student.op.ac.nz>
2023-02-02 22:15:10 +1300

    docs(readme.md): update readme file
    re #8

--- api/v1/test/08-basic-user-quiz.test.js
+++ api/v1/test/08-basic-user-quiz.test.js
@@ -1,22 +1,21 @@
-import chai from "chai";
-import chaiHttp from "chai-http";
-import { describe, it } from "mocha";
-import app from "../../../app.js";
-import PATHS from "../constants/paths.js";
-import { BASICTESTUSER } from "../../../utils/unitTestDataRequests.js";
+// import chai from "chai";
+// import chaiHttp from "chai-http";
+// import { describe, it } from "mocha";
+// import app from "./app.js";
+// import PATHS from "./api/v1/constants/paths.js";
+// import { BASICTESTUSER } from "./utils/unitTestDataRequests.js";
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-const BASE = "/api/v1";
-let token;
+// const BASE = "/api/v1";
 
-describe("Basic User Quiz Request Tests", () => {
-  describe("Sign On To Play A Quiz Request Test", () => {});
-  describe("Submit Quiz Answers Request Test", () => {});
-  describe("Delete Quiz Request Test", () => {});
-  describe("Admin User Quiz Request Error Handling Tests", () => {
-    describe("Create Quiz Request Test w/ Errors", () => {});
-    describe("Update Quiz Request Test w/ Errors", () => {});
-    describe("Delete Quiz Request Test w/ Errors", () => {});
-  });
-});
+// describe("Basic User Quiz Request Tests", () => {
+//   describe("Sign On To Play A Quiz Request Test", () => {});
+//   describe("Submit Quiz Answers Request Test", () => {});
+//   describe("Delete Quiz Request Test", () => {});
+//   describe("Admin User Quiz Request Error Handling Tests", () => {
+//     describe("Create Quiz Request Test w/ Errors", () => {});
+//     describe("Update Quiz Request Test w/ Errors", () => {});
+//     describe("Delete Quiz Request Test w/ Errors", () => {});
+//   });
+// });

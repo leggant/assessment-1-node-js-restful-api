84dec73d553d773705e48f706e9a9e2e33afa4ee
Anthony Legg <leggtc1@student.op.ac.nz>
2023-02-04 16:15:30 +1300

    fix(schema.prisma): fix bug in player delete
    from testing i found that admins could not delete basic players if the player has played a quiz.
    update the cascade rules which allow for users to be deleted by admins
    
    re #6, re #14

--- api/v1/test/01-reset-test-data.test.js
+++ api/v1/test/01-reset-test-data.test.js
@@ -6,23 +6,23 @@ import {
 } from "../../../utils/unitTestDataRequests.js";
 
 // Before each test, clear all test data
-before((done) => {
-  const result = Promise.all([
-    deleteTestAdminUser(),
-    deleteTestBasicUser(),
-    deleteTestQuizzes(),
-  ]);
-  result.then(() => {
-    done();
-  });
-});
+// before((done) => {
+//   const result = Promise.all([
+//     deleteTestQuizzes(),
+//     deleteTestAdminUser(),
+//     // deleteTestBasicUser(),
+//   ]);
+//   result.then(() => {
+//     done();
+//   });
+// });
 
 // After each test, clear all test data
 after((done) => {
   const result = Promise.all([
+    deleteTestQuizzes(),
     deleteTestAdminUser(),
     deleteTestBasicUser(),
-    deleteTestQuizzes(),
   ]);
   result.then(() => {
     console.info("Test Users and Quizzes Deleted");
---
+++ prisma/migrations/20230204030140_dev/migration.sql
@@ -0,0 +1,160 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BASIC_USER', 'ADMIN_USER');
+
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('easy', 'medium', 'hard');
+
+-- CreateEnum
+CREATE TYPE "AnswerType" AS ENUM ('boolean', 'multiple');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profileImgURL" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BASIC_USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlockedToken" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "exp" BIGINT NOT NULL,
+
+    CONSTRAINT "BlockedToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlockedUser" (
+    "id" SERIAL NOT NULL,
+    "uid" TEXT NOT NULL,
+    "exp" BIGINT NOT NULL,
+
+    CONSTRAINT "BlockedUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserScore" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "quizId" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserScore_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserParticipate" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "quizId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserParticipate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserQuestionAnswer" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "quizId" INTEGER NOT NULL,
+    "questionId" INTEGER NOT NULL,
+    "answer" TEXT NOT NULL,
+    "correctAnswer" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserQuestionAnswer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "quizId" INTEGER,
+    "question" TEXT NOT NULL,
+    "correctAnswer" TEXT NOT NULL,
+    "incorrectAnswers" JSONB NOT NULL,
+    "possibleAnswers" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Quiz" (
+    "id" SERIAL NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "answerType" "AnswerType" NOT NULL DEFAULT 'multiple',
+    "difficulty" "Difficulty" NOT NULL DEFAULT 'easy',
+    "numQuestions" INTEGER NOT NULL DEFAULT 10,
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Quiz_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_profileImgURL_key" ON "User"("profileImgURL");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserQuestionAnswer_userId_quizId_questionId_key" ON "UserQuestionAnswer"("userId", "quizId", "questionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "UserScore" ADD CONSTRAINT "UserScore_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserScore" ADD CONSTRAINT "UserScore_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserParticipate" ADD CONSTRAINT "UserParticipate_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserParticipate" ADD CONSTRAINT "UserParticipate_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserQuestionAnswer" ADD CONSTRAINT "UserQuestionAnswer_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserQuestionAnswer" ADD CONSTRAINT "UserQuestionAnswer_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserQuestionAnswer" ADD CONSTRAINT "UserQuestionAnswer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Quiz" ADD CONSTRAINT "Quiz_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE NO ACTION ON UPDATE CASCADE;

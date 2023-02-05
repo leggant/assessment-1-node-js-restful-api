9b3cbddbe1d7fb08e2b23e47a9e0c531ddcf5295
Anthony Legg <leggtc1@student.op.ac.nz>
2023-02-05 14:59:09 +1300

    docs(api/v1/*): add jsDoc code to controllers
    add jsDoc also to the PATHs constants file
    
    re #13

--- api/v1/controllers/userProfileController.js
+++ api/v1/controllers/userProfileController.js
@@ -1,5 +1,4 @@
 import checkDataType from "../../../utils/checkDataType.js";
-
 import {
   getSingleUserById,
   getSingleUserByParam,
@@ -10,20 +9,50 @@ import {
   clearBlockedUsers,
 } from "../../../utils/userRequests.js";
 
+/**
+ * Get user profile information through either the token provided in the request
+ * for the current authenticated users data; or, if an admin, query a basic user
+ * by their email or username values
+ * @function ctGetUser
+ * @async
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {Response} res
+ */
 const ctGetUser = async (req, res) => {
   try {
+    /**
+     * query is assigned data by the mw_userProfile middleware
+     * which will provide the current users id as a string by default
+     * If an admin uses this path with a query of another "basic" user
+     * email or username, their token is first validated, if ok, the param is assigned
+     * to the req.profileQuery as an object
+     * @constant query
+     * @type {string|{userName: string}|{email: string}}
+     * @requires string|object
+     */
     const query = req.profileQuery;
-    // user can either be a string, or an object with a single key value pair
-    let type = checkDataType(query);
-    const queryById = type === "string";
+    /**
+     * check if the data type of query is a string
+     * ie. the current user is querying their own data
+     * @constant {Boolean} queryById
+     */
+    const queryById = typeof query === "string";
     const userData = await (async () => {
+      /**
+       * if queryById is a string query user by id
+       * otherwise query the user with the object email/username: string
+       */
       const data = queryById
         ? await getSingleUserById(query)
         : await getSingleUserByParam(query);
       const datares = await data;
+      /**
+       * @returns {UserQuery}
+       */
       return datares;
     })();
-    type = checkDataType(userData);
+    const type = checkDataType(userData);
     const noData = type === "undefined" || type === null;
     if (noData) {
       return res.status(404).json({
@@ -43,6 +72,18 @@ const ctGetUser = async (req, res) => {
   }
 };
 
+/**
+ * Get user profile information through either the token provided in the request
+ * for the current authenticated users data; or, if an admin, query a basic user
+ * by their email or username values
+ * @function ctUpdateUser
+ * @async
+ * @param {Request} req
+ * @param {Object} req.user - currently authenticated user data
+ * @param {Object} req.body - user profile data to change
+ * @param {Response} res - return update success status or an error output on failure
+ * @returns {Response} res
+ */
 const ctUpdateUser = async (req, res) => {
   try {
     // eslint-disable-next-line prefer-destructuring
